87fb92218245b3dfba87d1db96c166197bc3af11
mahedee <mahedee.hasan@gmail.com>
2017-08-31 12:54:03 +0600

    added readme

---
+++ src/HRMBot/Dialogs/RootLuisDialog1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading.Tasks;
+
+namespace HRMBot.Dialogs
+{
+    //[LuisModel("6f13a4ce-dabe-485a-bb3f-9aba3156ea95", "5ea418ae538a402a9b99a936389fd0e7", domain: "westus.api.cognitive.microsoft.com", staging: true)]
+    //[Serializable]
+    public partial class RootLuisDialog : LuisDialog<object>
+    {
+      
+    }    
+}
