e940935e88edb78e8603133a649fd19542e6474a
Ratan Sunder Parai <ratanparai@gmail.com>
2017-09-02 20:59:48 +0600

    initial step to integrate database storate with the main bot application

---
+++ src/HRMBot.Storage/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMBot.Storage.Models
+{
+    class Employee
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<MobileNumber> MobileNumbers { get; set; }
+        
+    }
+}
---
+++ src/HRMBot.Storage/Models/MobileNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMBot.Storage.Models
+{
+    class MobileNumber
+    {
+        [Display(Name = "Mobile Number")]
+        [Required]
+        public int Number { get; set; }
+
+        public virtual Employee Employee { get; set; }
+    }
+}
