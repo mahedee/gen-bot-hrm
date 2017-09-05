ef8dcaafd84f05806d71e48b411a624d5fec396c
Ratan Sunder Parai <ratanparai@gmail.com>
2017-09-05 08:38:55 +0600

    Initial commit of webui

---
+++ src/HRMBot.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRMBot.Web.Startup))]
+namespace HRMBot.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
