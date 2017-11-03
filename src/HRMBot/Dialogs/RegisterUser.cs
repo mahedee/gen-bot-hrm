7e5dffcfe8eedad48e96aaefa5af061bc680666f
Ratan Sunder Parai <ratanparai@gmail.com>
2017-11-03 17:17:52 +0600

    fix project and solution settings

---
+++ src/HRMBot/Dialogs/RegisterUser.cs
@@ -0,0 +1,31 @@
+﻿using HRMBot.Extensions;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using HRMBot.Repository;
+
+namespace HRMBot.Dialogs
+{
+    public partial class RootLuisDialog
+    {
+
+        [LuisIntent("RegisterUser")]
+        public async Task RegisterUser(IDialogContext context, LuisResult result)
+        {
+
+            string message = context.PrivateConversationData.ToString();
+            
+
+
+            await context.PostAsync(message);
+            context.Wait(this.MessageReceived);
+
+
+        }
+
+    }
+}
