b5b49cc0ba5d6388bb4ad6dc67072b4db6ddd6aa
Ratan Sunder Parai <ratanparai@gmail.com>
2017-11-03 20:44:26 +0600

    trying to integrate conversation flow

---
+++ src/HRMBot/Dialogs/MobileNumberDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace HRMBot.Dialogs
+{
+    public class MobileNumberDialog : IDialog<int>
+    {
+        private int _attempts = 3;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("To verify please send your mobile number in 01XXXXXXXXX format. example:- 01771998817");
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+
+            int mobileNumber;
+
+            if (Int32.TryParse(message.Text, out mobileNumber) && (mobileNumber == 10))
+            {
+                context.Done(mobileNumber);
+            }
+            else
+            {
+                --_attempts;
+                if (_attempts > 0 && message.Text.Equals("cancel"))
+                {
+                    context.Fail(new OperationCanceledException(""));   
+                }
+                if (_attempts > 0)
+                {
+                    await context.PostAsync("I'm sorry, I don't understand your reply. What is your Mobile number (e.g. '01771998817')?");
+
+                    context.Wait(this.MessageReceivedAsync);
+                }
+                else
+                {
+                    context.Fail(new TooManyAttemptsException("Message was not a valid mobile number."));
+                }
+            }
+        }
+    }
+}
--- src/HRMBot/Dialogs/RegisterUser.cs
+++ src/HRMBot/Dialogs/RegisterUser.cs
@@ -16,15 +16,47 @@ namespace HRMBot.Dialogs
         [LuisIntent("RegisterUser")]
         public async Task RegisterUser(IDialogContext context, LuisResult result)
         {
+            var previousIntent = context.PrivateConversationData.GetValueOrDefault<string>("PreviousIntent");
 
-            string message = context.PrivateConversationData.ToString();
-            
+            if (previousIntent == null)
+            {
+                context.PrivateConversationData.SetValue("PreviousIntent", "RegisterUser");
 
+            }
 
+            context.Call(new MobileNumberDialog(), PhoneNumberDialogResumeAfter);
+
+            //var activity = context.Activity.;
+            const string message = "To verify please send your mobile number in 01XXXXXXXXX format. example:- 01771998817";
             await context.PostAsync(message);
+
+            // start new dialog to get the mobile number
+
             context.Wait(this.MessageReceived);
 
 
+        }
+
+        private async Task PhoneNumberDialogResumeAfter(IDialogContext context, IAwaitable<int> result)
+        {
+            try
+            {
+                var mobileNumber = await result;
+
+                // send varification message
+            }
+            catch (TooManyAttemptsException)
+            {
+                await context.PostAsync("I'm sorry, I'm having issues understanding you.");
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            finally
+            {
+                context.Wait(this.MessageReceived);
+            }
         }
 
     }
